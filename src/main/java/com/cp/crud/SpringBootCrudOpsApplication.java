a7785dcff366d11e92136de7fe16dc326143d0af
nehanavrange <nehanavrange94@gmail.com>
2022-01-03 13:22:01 +0530

    first commit

---
+++ src/main/java/com/cp/crud/SpringBootCrudOpsApplication.java
@@ -0,0 +1,13 @@
+package com.cp.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudOpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCrudOpsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cp/crud/SpringBootCrudOpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cp.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudOpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
